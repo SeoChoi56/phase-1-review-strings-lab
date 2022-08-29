5d3db6c9054f128e96e3e69c9be41938defaa514
Seo Choi <seochoi56@gmail.com>
2022-08-29 11:15:04 -0400

    commit tree af0ffa9587f3039b1da765dc32b124b08017c1e4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Seo Choi'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+//const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+//user created
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//lab method
+
+//const shortGreeting = `Welcome, ${currentUser[0]}!`
+//user created 
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
